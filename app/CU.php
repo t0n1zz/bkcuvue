33e96760e1f19c0519c9f209d1341519a09faf3c
t0n1zz <tony762843@yahoo.co.id>
2020-01-09 09:00:18 +0700

    fixing aktivis data profile bug

--- app/CU.php
+++ app/CU.php
@@ -7,7 +7,7 @@ use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Cviebrock\EloquentSluggable\Sluggable;
 
-class Cu extends BaseEloquent {
+class Cu extends Model {
     
     use \Venturecraft\Revisionable\RevisionableTrait;
     use Dataviewer, LogsActivity, SoftDeletes, Sluggable;
