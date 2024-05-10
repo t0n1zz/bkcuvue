c2369fadd88541b403b4376e7e27c1286a309c38
wandycandra <wandyhc@gmail.com>
2024-05-10 14:45:46 +0700

    update format laporan monitoring

--- app/Exports/Monitoring.php
+++ app/Exports/Monitoring.php
@@ -2,11 +2,8 @@
 
 namespace App\Exports;
 
-use App\HariLibur;
 use App\Monitoring as AppMonitoring;
-use App\PresensiCuti;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromArray;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithCustomStartCell;
@@ -39,7 +36,7 @@ class Monitoring implements FromArray, WithHeadings, WithCustomStartCell, WithEv
                 /** @var Sheet $sheet */
                 $sheet = $event->sheet;
 
-                $sheet->mergeCells('A1:K1');
+                $sheet->mergeCells('A1:N1');
                 $sheet->setCellValue('A2', "Jlh. Rekomendasi");
 
                 $sheet->setCellValue('B2', "Jlh. Tercapai");
@@ -50,17 +47,23 @@ class Monitoring implements FromArray, WithHeadings, WithCustomStartCell, WithEv
 
                 $sheet->setCellValue('E2', "Rekomendasi");
 
-                $sheet->setCellValue('F2', "TP");
+                $sheet->setCellValue('F2', "Pencapaian");
 
-                $sheet->setCellValue('G2', "Jenis");
+                $sheet->setCellValue('G2', "Kendala");
 
-                $sheet->setCellValue('H2', "Perspektif");
+                $sheet->setCellValue('H2', "Tindak Lanjut");
 
-                $sheet->setCellValue('I2', "PIC CU");
+                $sheet->setCellValue('I2', "TP");
 
-                $sheet->setCellValue('J2', "PIC PUSKOPCUINA");
+                $sheet->setCellValue('J2', "Jenis");
 
-                $sheet->setCellValue('K2', "Tgl. Monitoring");
+                $sheet->setCellValue('K2', "Perspektif");
+
+                $sheet->setCellValue('L2', "PIC CU");
+
+                $sheet->setCellValue('M2', "PIC PUSKOPCUINA");
+
+                $sheet->setCellValue('N2', "Tgl. Monitoring");
 
 
 
@@ -91,34 +94,41 @@ class Monitoring implements FromArray, WithHeadings, WithCustomStartCell, WithEv
 
 
                 $event->sheet->getDelegate()->getStyle('A1:K1')->applyFromArray($styleArray);
-                $event->sheet->getDelegate()->getStyle('A1:A100')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getStyle('A1:A1000')->applyFromArray($styleArray3);
                 $event->sheet->getDelegate()->getColumnDimension('A')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('B1:B100')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getStyle('B1:B1000')->applyFromArray($styleArray3);
                 $event->sheet->getDelegate()->getColumnDimension('B')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('C1:C100')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getStyle('C1:C1000')->applyFromArray($styleArray3);
                 $event->sheet->getDelegate()->getColumnDimension('C')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('D3:D100')->applyFromArray($styleArray2);
+                $event->sheet->getDelegate()->getStyle('D3:D1000')->applyFromArray($styleArray2);
                 $event->sheet->getDelegate()->getColumnDimension('D')->setWidth(50);
-                $event->sheet->getDelegate()->getStyle('E3:E100')->applyFromArray($styleArray2);
+                $event->sheet->getDelegate()->getStyle('E3:E1000')->applyFromArray($styleArray2);
                 $event->sheet->getDelegate()->getColumnDimension('E')->setWidth(50);
-                $event->sheet->getDelegate()->getStyle('F1:F100')->applyFromArray($styleArray3);
-                $event->sheet->getDelegate()->getColumnDimension('F')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('G1:G100')->applyFromArray($styleArray3);
-                $event->sheet->getDelegate()->getColumnDimension('G')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('H1:H100')->applyFromArray($styleArray3);
-                $event->sheet->getDelegate()->getColumnDimension('H')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('I1:I100')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getStyle('F1:F1000')->applyFromArray($styleArray2);
+                $event->sheet->getDelegate()->getColumnDimension('F')->setWidth(50);
+                $event->sheet->getDelegate()->getStyle('G1:G1000')->applyFromArray($styleArray2);
+                $event->sheet->getDelegate()->getColumnDimension('G')->setWidth(50);
+                $event->sheet->getDelegate()->getStyle('H1:H1000')->applyFromArray($styleArray2);
+                $event->sheet->getDelegate()->getColumnDimension('H')->setWidth(50);
+                $event->sheet->getDelegate()->getStyle('I1:I1000')->applyFromArray($styleArray2);
                 $event->sheet->getDelegate()->getColumnDimension('I')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('J1:J100')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getStyle('J1:J1000')->applyFromArray($styleArray3);
                 $event->sheet->getDelegate()->getColumnDimension('J')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('K1:K100')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getStyle('K1:K1000')->applyFromArray($styleArray3);
                 $event->sheet->getDelegate()->getColumnDimension('K')->setAutoSize(true);
-                $event->sheet->getDelegate()->getStyle('A1:K1')->applyFromArray(['fill' => [
+                $event->sheet->getDelegate()->getStyle('L1:L1000')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getColumnDimension('L')->setAutoSize(true);
+                $event->sheet->getDelegate()->getStyle('M1:M1000')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getColumnDimension('M')->setAutoSize(true);
+                $event->sheet->getDelegate()->getStyle('N1:N1000')->applyFromArray($styleArray3);
+                $event->sheet->getDelegate()->getColumnDimension('N')->setAutoSize(true);
+
+                $event->sheet->getDelegate()->getStyle('A1:N1')->applyFromArray(['fill' => [
                     'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
                     'color' => ['argb' => 'ffff00']
                 ]]);
 
-                $event->sheet->getStyle('A1:K1')->applyFromArray([
+                $event->sheet->getStyle('A1:N1')->applyFromArray([
                     'borders' => [
                         'allBorders' => [
                             'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
@@ -137,79 +147,115 @@ class Monitoring implements FromArray, WithHeadings, WithCustomStartCell, WithEv
 
     public function array(): array
     {
-        // $date = \Carbon\Carbon::parse($this->periode . "-" . '01' . "-01");
-        $model = null;
-        if($this->id_tp !='semua'){
-            $model = AppMonitoring::with('cu', 'tp', 'aktivis_cu', 'aktivis_bkcu', 'monitoring_rekom', 'monitoring_pencapaian','monitoring_rekom_ok')->where('id_cu', $this->id_cu)->where('id_tp',$this->id_tp)->get()->toArray();
-        }else{
-            $model = AppMonitoring::with('cu', 'tp', 'aktivis_cu', 'aktivis_bkcu', 'monitoring_rekom', 'monitoring_pencapaian','monitoring_rekom_ok')->where('id_cu', $this->id_cu)->get()->toArray();
-        }
+        try {
+            //code...
+            $model = null;
+            if($this->id_tp !='semua'){
+                $model = AppMonitoring::with('cu', 'tp', 'aktivis_cu', 'aktivis_bkcu', 'monitoring_rekom', 'monitoring_pencapaian','monitoring_rekom_ok')->where('id_cu', $this->id_cu)->where('id_tp',$this->id_tp)->get()->toArray();
+            }else{
+                $model = AppMonitoring::with('cu', 'tp', 'aktivis_cu', 'aktivis_bkcu', 'monitoring_rekom', 'monitoring_pencapaian','monitoring_rekom_ok')->where('id_cu', $this->id_cu)->get()->toArray();
+            }
+        
+            // dd($model[0]['monitoring_pencapaian']);
+            $arrayComplete = [];
+            foreach ($model as $monitoring) {
+
+                $count = 0;
+                $arrayNew = [];
+                $arrayRekom = [];
+                $arrayPencapaian =[];
+                $arrayKendala = [];
+                $arrayTindak = [];
+
+                foreach ($monitoring['monitoring_rekom'] as $rekom) {
+                    array_push($arrayRekom, $rekom['rekomendasi']);
+                }
+
+                foreach ($monitoring['monitoring_pencapaian'] as $pencapaian) {
+
+                    array_push($arrayPencapaian, $pencapaian['pencapaian']);
+                    if(isset($pencapaian['kendala'])){
+                        array_push($arrayKendala,$pencapaian['kendala']);
+                    }
+
+                    if(isset($pencapaian['tindak'])){
+                        array_push($arrayTindak,$pencapaian['tindak']);
+                    }
+
+                }
     
-        $arrayComplete = [];
-        foreach ($model as $monitoring) {
-            // if (isset($cutiS->groupBy('id_user')[$aktivis->id])) {
-            $count = 0;
-            $arrayNew = [];
-            $arrayRekom = [];
-            foreach ($monitoring['monitoring_rekom'] as $rekom) {
-                array_push($arrayRekom, $rekom['rekomendasi']);
-            }
+                if(isset($monitoring['monitoring_rekom'])){
+                    array_push($arrayNew, count($monitoring['monitoring_rekom']));
+                }else{
+                    array_push($arrayNew, '0');
+                }
+    
+                if(isset($monitoring['monitoring_rekom_ok'])){
+                    array_push($arrayNew, count($monitoring['monitoring_rekom_ok']));
+                }else{
+                    array_push($arrayNew, '0');
+                }
+    
+                if(isset($monitoring['monitoring_pencapaian'])){
+                    array_push($arrayNew, count($monitoring['monitoring_pencapaian']));
+                }else{
+                    array_push($arrayNew, '0');
+                }
+    
+                if(isset($monitoring['name'])){
+                    array_push($arrayNew, $monitoring['name']);
+                }else{
+                    array_push($arrayNew, '0');
+                }
 
-            if(isset($monitoring['monitoring_rekom'])){
-                array_push($arrayNew, count($monitoring['monitoring_rekom']));
-            }else{
-                array_push($arrayNew, '0');
-            }
+                if(isset($arrayRekom)){
+                    array_push($arrayNew, implode("\n", $arrayRekom));
+                }else{
+                    array_push($arrayNew, '-');
+                }
 
-            if(isset($monitoring['monitoring_rekom_ok'])){
-                array_push($arrayNew, count($monitoring['monitoring_rekom_ok']));
-            }else{
-                array_push($arrayNew, '0');
-            }
+                if(isset($arrayPencapaian)){
+                    array_push($arrayNew, implode("\n", $arrayPencapaian));
+                }else{
+                    array_push($arrayNew, '-');
+                }
 
-            if(isset($monitoring['monitoring_pencapaian'])){
-                array_push($arrayNew, count($monitoring['monitoring_pencapaian']));
-            }else{
-                array_push($arrayNew, '0');
-            }
+                if(isset($arrayKendala)){
+                    array_push($arrayNew, implode("\n", $arrayKendala));
+                }else{
+                    array_push($arrayNew, '-');
+                }
 
-            if(isset($monitoring['name'])){
-                array_push($arrayNew, $monitoring['name']);
-            }else{
-                array_push($arrayNew, '0');
+                if(isset($arrayTindak)){
+                    array_push($arrayNew, implode("\n", $arrayTindak));
+                }else{
+                    array_push($arrayNew, '-');
+                }
+               
+                if(isset($monitoring['tp']['name'])){
+                    array_push($arrayNew, $monitoring['tp']['name']);
+                }else{
+                    array_push($arrayNew, '-');
+                }
+                array_push($arrayNew, $monitoring['jenis']);
+                array_push($arrayNew, $monitoring['perspektif']);
+                if(isset($monitoring['aktivis_cu']['name'])){
+                    array_push($arrayNew, $monitoring['aktivis_cu']['name']);
+                }else{
+                    array_push($arrayNew, '-');
+                }
+                array_push($arrayNew, $monitoring['aktivis_bkcu']['name']);
+                array_push($arrayNew, Carbon::parse($monitoring['tanggal'])->format('d-m-Y'));
+                array_push($arrayComplete, $arrayNew);
+    
+                $count++;
             }
-            if(isset($arrayRekom)){
-                array_push($arrayNew, implode("\n", $arrayRekom));
-            }else{
-                array_push($arrayNew, '-');
-            }
-           
-            if(isset($monitoring['tp']['name'])){
-                array_push($arrayNew, $monitoring['tp']['name']);
-            }else{
-                array_push($arrayNew, '-');
-            }
-            array_push($arrayNew, $monitoring['jenis']);
-            array_push($arrayNew, $monitoring['perspektif']);
-            array_push($arrayNew, $monitoring['aktivis_cu']['name']);
-            array_push($arrayNew, $monitoring['aktivis_bkcu']['name']);
-            array_push($arrayNew, Carbon::parse($monitoring['tanggal'])->format('d-m-Y'));
-            array_push($arrayComplete, $arrayNew);
-
-            $count++;
+    
+            return $arrayComplete;
+        } catch (\Throwable $th) {
+            //throw $th;
         }
-
-
-
-        //     } else {
-        //         $arrayNew = [];
-        //         array_push($arrayNew, 0);
-        //         array_push($arrayNew, 0);
-        //         array_push($arrayComplete, $arrayNew);
-        //     }
-        // }
-
-        return $arrayComplete;
+       
     }
 
 
